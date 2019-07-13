b74e2dccef199c83996a16f2ae049b8f11cf23b7
stuart_zhang <stuartpage@qq.com>
2019-07-13 22:26:35 +0800

    也删除 css 的 sourcemap 文件。 调用 webpack 4 插件接口

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
+const fs = require('fs-extra');
+const co = require('co');
+const pkg = require('./package.json');
 class DeleteSourceMapWebpackPlugin{
     apply(compiler){
         let countMatchAssets = 0;
-        compiler.plugin('after-compile', (compilation, cb) => {
-            Object.keys(compilation.assets).filter(key => /\.js$/.test(key))
-                .forEach(key => {
-                    countMatchAssets += 1;
-                    const asset = compilation.assets[key];
-                    const source = asset.source().replace(/# sourceMappingURL=(.*\.map)/g, '# $1');
-                    compilation.assets[key] = Object.assign(asset, {
-                        source(){
-                            return source;
-                        }
-                    });
+        compiler.hooks.afterCompile.tap(pkg.name, compilation => {
+            Object.keys(compilation.assets).filter(key => /\.(?:js|css)$/.test(key)).forEach(key => {
+                countMatchAssets += 1;
+                const asset = compilation.assets[key];
+                const source = asset.source().replace(/# sourceMappingURL=(.*\.map)/g, '# $1');
+                compilation.assets[key] = Object.assign(asset, {
+                    source(){
+                        return source;
+                    }
                 });
-            cb();
+            });
         });
-  
-        compiler.plugin('done', stats => {
-            const fs = require('fs');
+        compiler.hooks.done.tapPromise(pkg.name, co.wrap(function *(stats){
             let countMatchMapAssets = 0;
-            Object.keys(stats.compilation.assets)
-                .filter(name => /\.js\.map$/.test(name))
-                .forEach(name => {
-                    countMatchMapAssets += 1;
+            yield Promise.all(Object.keys(stats.compilation.assets)
+                .filter(name => /\.(?:js|css)\.map$/.test(name))
+                .map(co.wrap(function *(name){
                     const {existsAt} = stats.compilation.assets[name];
-                    fs.unlinkSync(existsAt);
-                });
-            console.log(`⭐⭐⭐removed source map url: ${countMatchAssets} asset(s) processed`);
-            console.log(`⭐⭐⭐deleted map file: ${countMatchMapAssets} asset(s) processed`);
-        });
+                    if (typeof existsAt === 'string' &&
+                      (yield fs.pathExists(existsAt)) &&
+                      (yield fs.stat(existsAt)).isFile()) {
+                        countMatchMapAssets += 1;
+                        yield fs.remove(existsAt);
+                    }
+                })));
+            console.log(`⭐ ⭐ ⭐ removed source map url: ${countMatchAssets} asset(s) processed`);
+            console.log(`⭐ ⭐ ⭐ deleted map file: ${countMatchMapAssets} asset(s) processed`);
+        }));
     }
 }
 
