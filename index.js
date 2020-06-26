ec52178c19718a2feaa32f4190d0b394de631096
stuart_zhang <stuartpage@qq.com>
2020-06-26 15:46:39 +0800

    两个目标： 1. 兼容于 webpack 3.x 2. 兼容于 webpack 4.x 的后期版本。即，compilation.assets[].source() 成员函数返回的是 Buffer 而不是 String。

--- index.js
+++ index.js
@@ -2,21 +2,32 @@ const fs = require('fs-extra');
 const co = require('co');
 const pkg = require('./package.json');
 class DeleteSourceMapWebpackPlugin{
-    apply(compiler){
-        let countMatchAssets = 0;
-        compiler.hooks.afterCompile.tap(pkg.name, compilation => {
-            Object.keys(compilation.assets).filter(key => /\.(?:js|css)$/.test(key)).forEach(key => {
-                countMatchAssets += 1;
-                const asset = compilation.assets[key];
-                const source = asset.source().replace(/# sourceMappingURL=(.*\.map)/g, '# $1');
-                compilation.assets[key] = Object.assign(asset, {
-                    source(){
-                        return source;
+    constructor(){
+        this.countMatchAssets = 0;
+    }
+    afterCompile(compilation){
+        Object.keys(compilation.assets).filter(key => /\.(?:js|css)$/.test(key)).forEach(key => {
+            this.countMatchAssets += 1;
+            const asset = compilation.assets[key];
+            let source = asset.source();
+            const isBuffer = Buffer.isBuffer(source);
+            if (isBuffer) {
+                source = source.toString();
+            }
+            source = source.replace(/# sourceMappingURL=(.*\.map)/g, '# $1');
+            compilation.assets[key] = Object.assign(asset, {
+                source(){
+                    if (isBuffer) {
+                        return Buffer.from(source);
                     }
-                });
+                    return source;
+                }
             });
         });
-        compiler.hooks.done.tapPromise(pkg.name, co.wrap(function *(stats){
+    }
+    done(stats){
+        const that = this;
+        return co(function *(){
             let countMatchMapAssets = 0;
             yield Promise.all(Object.keys(stats.compilation.assets)
                 .filter(name => /\.(?:js|css)\.map$/.test(name))
@@ -29,9 +40,23 @@ class DeleteSourceMapWebpackPlugin{
                         yield fs.remove(existsAt);
                     }
                 })));
-            console.log(`⭐ ⭐ ⭐ removed source map url: ${countMatchAssets} asset(s) processed`);
+            console.log(`\n⭐ ⭐ ⭐ removed source map url: ${that.countMatchAssets} asset(s) processed`);
             console.log(`⭐ ⭐ ⭐ deleted map file: ${countMatchMapAssets} asset(s) processed`);
-        }));
+        });
+    }
+    apply(compiler){
+        if (compiler.hooks) {
+            compiler.hooks.afterCompile.tap(pkg.name, compilation => this.afterCompile(compilation));
+            compiler.hooks.done.tapPromise(pkg.name, stats => this.done(stats));
+        } else if (compiler.plugin) {
+            compiler.plugin('after-compile', (compilation, cb) => {
+                this.afterCompile(compilation);
+                cb();
+            });
+            compiler.plugin('done', stats => this.done(stats));
+        } else {
+            throw new Error('不被支持的 webpack 版本');
+        }
     }
 }
 
