0aa8a0255c4bc2ebd404099e00f9462478c78366
Tomáš Hruška <hruska@btl.cz>
2021-01-18 10:17:26 +0100

    Remove unnecessary code and files

--- README.md
+++ README.md
@@ -1,30 +1,12 @@
-# delete-sourcemap-webpack-plugin-stzhang
-
-与[sentry.io](https://sentry.io/)`javascript sdk`配套使用。在向[sentry.io](https://sentry.io/)上传`source-map`文件之后，再删除`source-map`文件与源文件内对`source-map`文件的引用注释指令。而不是，直接从`webpack`配置彻底关闭`source-map`生成。
-
-## 安装
-
-```npm i -D delete-sourcemap-webpack-plugin-stzhang```
-
-## 使用
-
 ```javascript
 // webpack.config.js
-const DeleteSourceMapWebpackPlugin = require('delete-sourcemap-webpack-plugin-stzhang')
+const DeleteSourceMapWebpackPlugin = require('delete-sourcemap-webpack-plugin')
 
 module.exports = {
-    // ...
-    plugins: [
-        new DeleteSourceMapWebpackPlugin()
-    ],
-    // ...
+     // ...
+     plugins: [
+         new DeleteSourceMapWebpackPlugin()
+     ],
+     // ...
 }
 ```
-
-## 解决缺陷
-
-### 支持去掉`css`文件的`source-map`
-
-### 当遇到内联`source-map`时，不再报`fs.unlink()`的删除文件失败错误
-
-### 调用`webpack 4`的`Plugin API`来接入插件
--- index.js
+++ index.js
@@ -3,27 +3,6 @@ const co = require('co');
 const pkg = require('./package.json');
 class DeleteSourceMapWebpackPlugin{
     constructor(){
-        this.countMatchAssets = 0;
-    }
-    afterCompile(compilation){
-        Object.keys(compilation.assets).filter(key => /\.(?:js|css)$/.test(key)).forEach(key => {
-            this.countMatchAssets += 1;
-            const asset = compilation.assets[key];
-            let source = asset.source();
-            const isBuffer = Buffer.isBuffer(source);
-            if (isBuffer) {
-                source = source.toString();
-            }
-            source = source.replace(/# sourceMappingURL=(.*\.map)/g, '# $1');
-            compilation.assets[key] = Object.assign(asset, {
-                source(){
-                    if (isBuffer) {
-                        return Buffer.from(source);
-                    }
-                    return source;
-                }
-            });
-        });
     }
     done(stats){
         const that = this;
@@ -40,22 +19,16 @@ class DeleteSourceMapWebpackPlugin{
                         yield fs.remove(existsAt);
                     }
                 })));
-            console.log(`\n⭐ ⭐ ⭐ removed source map url: ${that.countMatchAssets} asset(s) processed`);
             console.log(`⭐ ⭐ ⭐ deleted map file: ${countMatchMapAssets} asset(s) processed`);
         });
     }
     apply(compiler){
         if (compiler.hooks) {
-            compiler.hooks.afterCompile.tap(pkg.name, compilation => this.afterCompile(compilation));
             compiler.hooks.done.tapPromise(pkg.name, stats => this.done(stats));
         } else if (compiler.plugin) {
-            compiler.plugin('after-compile', (compilation, cb) => {
-                this.afterCompile(compilation);
-                cb();
-            });
             compiler.plugin('done', stats => this.done(stats));
         } else {
-            throw new Error('不被支持的 webpack 版本');
+            throw new Error('Unsupported Webpack version');
         }
     }
 }
